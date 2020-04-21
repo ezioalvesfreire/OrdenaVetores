6285dc3fb40ca8600f57e4a8d472623902101b84
Ézio Alves <ezio.freire@catolicasc.org.br>
2020-04-21 20:28:53 -0300

    adicionado a função busca binária

--- ordenacaoInsertionSort/main.cpp
+++ ordenacaoInsertionSort/main.cpp
@@ -3,6 +3,26 @@
 #include <conio.h>
 #include <time.h>
 
+int buscaBinaria(int arranjo[],int tamanho, int valorProcurado) {
+	// Na busca binária, espera-se que o vetor esteja ordenado
+
+	int i = 0;
+	int f = tamanho -1;
+	int meio;
+	while (i<=f) {
+		meio = (i+f)/2;
+		if (arranjo[meio]==valorProcurado)
+			return meio;
+		else {
+			if (valorProcurado>arranjo[meio])
+				i=meio+1;
+			else
+				f=meio-1;
+		}
+	}
+	return -1;
+}
+
 void insertion(int vet[], int n)
 {
 
@@ -51,8 +71,8 @@ void calculaTempo(){
 
 int main()
 {
-    int vetor[100000];
-    int n = 100000;
+    int vetor[10];
+    int n = 10;
 
     printf("\n================= ORDENACAO DE DADOS METODO INSERTION SORT ===================");
     printf("\n\n\n");
@@ -74,6 +94,19 @@ int main()
    imprimeVetor(vetor, n);
 
     printf("\n\n");
+
+    int p,resposta;
+    printf("Qual valor a ser procurado?\n");
+
+	scanf("%d", &p);
+
+	resposta=buscaBinaria(vetor,n,p);
+	if (resposta==-1)
+        printf("O valor procurado nao foi encontrado.");
+
+	else
+        printf("O valor procurado encontra-se na posicao %d ", resposta);
+        printf("\n\n");
 
     calculaTempo();
 
