7bc8bfe8da19c3372f8f780986af6eb500294997
Ézio Alves <ezio.freire@catolicasc.org.br>
2020-04-21 20:46:48 -0300

    adicionado a função busca binária no programa QuickSort

--- ordenacaoQuickSort/main.cpp
+++ ordenacaoQuickSort/main.cpp
@@ -3,7 +3,25 @@
 #include <conio.h>
 #include <time.h>
 
+int buscaBinaria(int arranjo[],int tamanho, int valorProcurado) {
+	// Na busca binária, espera-se que o vetor esteja ordenado
 
+	int i = 0;
+	int f = tamanho -1;
+	int meio;
+	while (i<=f) {
+		meio = (i+f)/2;
+		if (arranjo[meio]==valorProcurado)
+			return meio;
+		else {
+			if (valorProcurado>arranjo[meio])
+				i=meio+1;
+			else
+				f=meio-1;
+		}
+	}
+	return -1;
+}
 
 int dividir(int vetor[], int esq, int dir)
 {
@@ -59,8 +77,8 @@ for(int i=0; i < n; i++){
 
 int main()
 {
-int vetor[100000];
-int n = 100000;
+int vetor[10];
+int n = 10;
     printf("\n================= ORDENACAO DE DADOS METODO QUICK SORT ===================\n\n");
 
      geraNumAleatorio(vetor, n);
@@ -78,6 +96,21 @@ int n = 100000;
 
 //================== IMPRIME VETOR DESORDENADO ====================================
     imprimeVetor(vetor, n);
+
+      printf("\n\n");
+
+     int p,resposta;
+    printf("Qual valor a ser procurado?\n");
+
+	scanf("%d", &p);
+
+	resposta=buscaBinaria(vetor,n,p);
+	if (resposta==-1)
+        printf("O valor procurado nao foi encontrado.");
+
+	else
+        printf("O valor procurado encontra-se na posicao %d ", resposta);
+        printf("\n\n");
 
     printf("\n\n\n");
 
